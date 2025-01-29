169f50b4f826915b13b8f8c0460023039d835608
husknet <127146284+husknet@users.noreply.github.com>
2025-01-29 13:32:11 -0400

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -4,6 +4,7 @@ import geoip from 'geoip-lite';
 // Known scraper ISPs
 const SCRAPER_ISPS = [
   "Barry Hamel Equipment Ltd",
+  "Linode",
   "Bayer AG, Germany, Leverkusen",
   "TeraGo Networks Inc.",
   "Microsoft Corporation",
