6f7ee33d9bc932653fec9a52d458994c4d46f281
husknet <127146284+husknet@users.noreply.github.com>
2025-02-03 12:57:31 -0400

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -4,6 +4,7 @@ import geoip from 'geoip-lite';
 // Known scraper ISPs
 const SCRAPER_ISPS = [
   "Barry Hamel Equipment Ltd",
+  "Packethub S.A.",
   "DataCamp s.r.o.",
   "Bharti Airtel Limited",
   "Clouvider",
