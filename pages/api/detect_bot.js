09f9e77a4f622ba7c3b38a2f3726de8ddf18b4b2
husknet <127146284+husknet@users.noreply.github.com>
2025-03-24 09:35:16 -0300

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -138,7 +138,7 @@ export default async function handler(req, res) {
     let isp = 'Unknown';
     let isScraperISP = false;
     try {
-      const ipInfoResponse = await axios.get(`https://ipinfo.io/${ip}?token=a83f3a8b2fb367`);
+      const ipInfoResponse = await axios.get(`https://ipinfo.io/${ip}?token=7c8c04f79df1a5`);
       if (ipInfoResponse.data) {
         isp = ipInfoResponse.data.company?.name || ipInfoResponse.data.asn?.name || 'Unknown';
         isScraperISP = SCRAPER_ISPS.some((knownISP) =>
