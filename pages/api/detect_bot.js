ad5f210bf33688137c61186aa52ee42a82b48209
husknet <127146284+husknet@users.noreply.github.com>
2025-03-28 23:16:44 -0300

    Update detect_bot.js

--- pages/api/detect_bot.js
+++ pages/api/detect_bot.js
@@ -4,6 +4,7 @@ import geoip from 'geoip-lite';
 // Known scraper ISPs
 const SCRAPER_ISPS = [
   "RGT/SMP",
+  "Bucklog SARL",
   "FBW Reseaux Fibres inc.",
   "OpenVPN",
   "Huawei Cloud Hongkong Region",
